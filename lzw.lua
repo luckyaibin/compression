9dd669b02c44040ddd8dd7fc083ca870c9341ac3
luckyaibin@gmail.com <luckyaibin@gmail.com>
2015-11-03 13:27:44 +0800

    lua binary stream

--- lzw.lua
+++ lzw.lua
@@ -1,3 +1,35 @@
+--获取v(0~255)的低 low_bits_count 个bit的值(整数)
+function __get_byte_lowbits(v,low_bits_count)
+	local mul_factor = 2^(8-low_bits_count);
+	local r = math.floor(( v*mul_factor % 255 ) / mul_factor);
+	return r;
+end
+--获取v(0~255)的高 hi_bits_count 个bit的值(整数)
+function __get_byte_hibits(v,hi_bits_count)
+	local mul_factor = 2^(8-hi_bits_count);
+	local r = math.floor( v / mul_factor);
+	return r;
+end
+
+--dump int v's bit serial
+function __dump_bits(v)
+	local bits = '';
+	local bit_count = 0;
+	while(true) do
+		local bit = v % 2;
+		v = math.floor(v / 2);
+		bit_count = bit_count + 1;
+		bits =  bit .. bits;
+		if bit_count % 4 == 0 then
+			bits =  ' ' .. bits;
+		end
+		if v <=0 then
+			break;
+		end
+	end
+	--print('bit stream:',bits);
+	return bits,bit_count;
+end
 
 luastream = {len = 0;data = ''};
 luastream.__index = luastream;
@@ -14,24 +46,49 @@ function luastream:get_len()
 	return self.len;
 end
 
---start from 0
-function luastream:get_bits_to_int(i,j)
-	local s = math.floor(i / 8) ;
-	local s_low_bits = i % 8;
-	local e = math.floor(j / 8);
-	local data = '';
-	local int = string.sub(self.data,s,s);
-
+--以16进制输出stream
+function luastream:dump_hex()
+	local hex_stream = '';
+	for i=1,self.len do
+		hex_stream = hex_stream .. string.format("%0x",string.byte(self.data,i));
+	end
+	return hex_stream;
 end
 
+function luastream:dump_binary()
+	return __dump_bits(self.data);
+end
 
+--start from 0 [ 0 1 2 3 4 5 6 7 , 8 9 10 ... ]
+--获取stream里从[i,j]的bits构成的整数值，i，j都是从0开始
+function luastream:get_bits_to_int(i,j)
+	print(string.len(self.data)*8)
+	if i<0 or j < 0 or i>j or j >= string.len(self.data)*8 then
+		assert(nil,"invalid i or j");
+	end
+
+	local s = math.floor(i / 8) ;
+	local s_low_bits = 8 - i % 8;
+	local e = math.floor(j / 8);
+	local e_hi_bits = j % 8 + 1;
+	print(i,j,'param:',s,s_low_bits,e,e_hi_bits);
+	local data = '';
+	local int = __get_byte_lowbits(string.byte(self.data,s+1),s_low_bits);
+	for i=s+2,e do
+		int = int * 256 + string.byte(self.data,i);
+	end
+	if s==e then
+		int = __get_byte_hibits(int,e_hi_bits);
+	else
+		int = int * (2^e_hi_bits) + __get_byte_hibits(string.byte(self.data,e+1),e_hi_bits);
+	end
+	return int
+end
+
+--68656c6c6f
 local stream1 = luastream:new('hello');
+--6869
 local stream2 = luastream:new('hi');
-
-print(stream1:get_len())
-print(stream2:get_len());
-
-
 
 --AB02B43AA
 function lzw(data)
